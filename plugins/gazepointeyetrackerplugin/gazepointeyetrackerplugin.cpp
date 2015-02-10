b6caea89be854e70dc39efb5646880dddeff085a
Luiz Romário Santana Rios <luizromario@gmail.com>
2015-02-10 00:24:09 -0300

    Add gazepoint plugin to the tree

---
+++ plugins/gazepointeyetrackerplugin/gazepointeyetrackerplugin.cpp
@@ -0,0 +1,92 @@
+#include "gazepointeyetrackerplugin.hpp"
+
+#include <QTimerEvent>
+
+#include <string>
+
+#include "rapidxml.hpp" // Because Qt's XML parser is ASS
+
+using namespace rapidxml;
+using namespace std;
+
+GazepointEyetrackerPlugin::GazepointEyetrackerPlugin()
+{}
+
+GazepointEyetrackerPlugin::~GazepointEyetrackerPlugin()
+{}
+
+QVector<BaseEyetrackerPlugin::Param> GazepointEyetrackerPlugin::availableTrackingParams() const
+{
+    return {
+        { "server", "Server Adderss", Param::String, {}, "127.0.0.1" },
+        { "port", "Port", Param::Int, { 1, 65535 }, 4242 }
+    };
+}
+
+bool GazepointEyetrackerPlugin::setTrackingParams(const QVariantHash &params)
+{
+    _server = params["server"].toString();
+    _port = params["port"].toInt();
+
+    return true;
+}
+
+unique_ptr<BaseTrackingCalibrationWidget> GazepointEyetrackerPlugin::trackingCalibrationWidget() const
+{
+    return nullptr;
+}
+
+bool GazepointEyetrackerPlugin::calibrateTracking(const QVector<EyesPosition> &)
+{
+    _socket.write(valCmd("CALIBRATE_START", true));
+    _socket.write(valCmd("CALIBRATE_SHOW", true));
+    return _socket.waitForBytesWritten();
+}
+
+bool GazepointEyetrackerPlugin::startTracking()
+{
+    _socket.connectToHost(_server, _port);
+    if (!_socket.waitForConnected())
+        return false;
+
+
+    _socket.write(valCmd("ENABLE_SEND_POG_BEST", true));
+    _socket.write(valCmd("ENABLE_SEND_POG_LEFT", true));
+    _socket.write(valCmd("ENABLE_SEND_POG_RIGHT", true));
+    _socket.write(valCmd("ENABLE_SEND_DATA", true));
+
+    _timerId = startTimer(20);
+    return _socket.waitForBytesWritten();
+}
+
+bool GazepointEyetrackerPlugin::stopTracking()
+{
+    killTimer(_timerId);
+    _socket.disconnectFromHost();
+    return _socket.waitForDisconnected();
+}
+
+template <class Ch>
+static double attr_val_d(xml_node<Ch> *node, const char *attr_name)
+{
+    auto attr = node->first_attribute(attr_name);
+    return stod(attr->value());
+}
+
+void GazepointEyetrackerPlugin::timerEvent(QTimerEvent *e)
+{
+    if (e->timerId() != _timerId)
+        return QObject::timerEvent(e);
+
+    auto all = _socket.readAll();
+
+    xml_document<> d;
+    d.parse<0>(all.data());
+
+    for (auto node = d.first_node("REC"); node; node = node->next_sibling("REC"))
+        emit eyesPositionChanged({
+            { attr_val_d(node, "BPOGX"), attr_val_d(node, "BPOGY") },
+            { attr_val_d(node, "LPOGX"), attr_val_d(node, "LPOGY") },
+            { attr_val_d(node, "RPOGX"), attr_val_d(node, "RPOGY") }
+        });
+}
---
+++ plugins/gazepointeyetrackerplugin/gazepointeyetrackerplugin.hpp
@@ -0,0 +1,50 @@
+#ifndef DUMMYEYETRACKERPLUGIN_HPP
+#define DUMMYEYETRACKERPLUGIN_HPP
+
+#include <baseeyetrackerplugin.hpp>
+
+#include <QTcpSocket>
+
+class GazepointEyetrackerPlugin : public QObject, public BaseEyetrackerPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(
+        IID "br.ufba.dcc.eyecommunicator-cepred.plugins.gazepoint"
+        FILE "gazepointeyetrackerplugin.json")
+
+    EYETRACKER_PLUGIN_INTERFACE(Gazepoint)
+
+signals:
+    void eyesPositionChanged(const EyesPosition &pos);
+
+protected:
+    void timerEvent(QTimerEvent *e);
+
+private:
+    enum class Cmd { GET, SET };
+
+    inline QByteArray xmlCmd(Cmd type, const QString &cmd, const QString &params = {}) const
+    {
+        return
+            (QString{"<"} + (type == Cmd::GET? "GET" : "SET") +
+            " ID=\"" + cmd + "\" " + params + ">\r\n").toLocal8Bit();
+    }
+    inline QByteArray valCmd(const QString &cmd, const QVariant &val) const
+    {
+        auto isBool = val.type() == QVariant::Bool;
+
+        return xmlCmd(
+            Cmd::SET,
+            cmd,
+            (isBool? "STATE" : "VALUE") +
+            QString{"=\""} +
+            (isBool? val.toBool()? "1" : "0" : val.toString()) + "\"");
+    }
+
+    QTcpSocket _socket;
+    QString _server;
+    int _port;
+    int _timerId;
+};
+
+#endif // DUMMYEYETRACKERPLUGIN_HPP
